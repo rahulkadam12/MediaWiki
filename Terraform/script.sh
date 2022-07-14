960c082a8137e6f0f845633871f544e0279bfe11
inkadamr <rahul.kadam@tetrapak.com>
2022-07-14 13:35:41 +0530

    Final commit

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# sleep until instance is ready
-#until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
-#  sleep 1
-#done
+#sleep until instance is ready
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+  sleep 1
+done
 
 # Update Packages and Install snapd
 
@@ -26,21 +26,18 @@ sleep 45
 
 microk8s status --wait-ready
 sleep 20
-alias kubectl="microk8s kubectl"
 
 # Cloning Git Repository and changing Directory
 cd /opt
-sudo git clone https://github.com/rahulkadam12/MediaWiki.git && cd MediaWiki && cd kubernetes
+sudo git clone https://github.com/rahulkadam12/MediaWiki.git && cd MediaWiki && cd Kubernetes
 
 # Creating pods using Kubectl commands for secrets, PV, Mariadb and Mediawiki deployment
 
-sudo kubectl create -f mariadb-secrets.yaml
-sudo kubectl create -f persistent-volumes.yaml
-sudo kubectl create -f mariadb-deployment.yaml
-sudo kubectl create -f mariadb-svc.yaml
-sudo kubectl create -f mediawikiapp-deployment.yaml
-sudo kubectl create -f mediawiki-svc.yaml
-
-
-# make sure nginx is started
-service nginx start
+sudo microk8s kubectl create -f mariadb-secrets.yaml
+sudo microk8s kubectl create -f persistent-volumes.yaml
+sudo microk8s kubectl create -f mariadb-deployment.yaml
+sudo microk8s kubectl create -f mariadb-svc.yaml
+sudo microk8s kubectl create -f mediawikiapp-deployment.yaml
+sudo microk8s kubectl create -f mediawiki-svc.yaml
+sleep 10
+sudo microk8s kubectl get pods
