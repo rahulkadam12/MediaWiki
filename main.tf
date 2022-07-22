ce36f4c735bd4e2a715c5398a3cb75e50de56a85
inkadamr <rahul.kadam@tetrapak.com>
2022-07-22 16:57:16 +0530

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
 terraform {
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
   cloud {
     organization = "My-Kubernetes-Test"
 
